0cbc75fba19ac8f235ea08e7fb4d471742a94e94
betterwithpesto <pat@prost.io>
2014-09-01 20:09:35 -0700

    changed ports for heroku

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ var app = express();
 
 app.use(express.static(__dirname));
 
-var server = app.listen(4000, function() {
-  console.log('Listening on port %d', server.address().port);
+// var server = app.listen(4000, function() {
+//   console.log('Listening on port %d', server.address().port);
+// });
+
+var port = process.env.PORT || 5000; // Use the port that Heroku provides or default to 5000
+app.listen(port, function() {
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
